0ddd0267548e6f0ec25310d949ae293d5fb30db8
Huda Yousif <hjdoski@gmail.com>
2019-10-01 18:55:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,12 @@
-def reverse_each_word(string)
-  { |b| "#{b.reverse} on #{"string".reverse" }
-end 
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  reversed_array = new_array.each {|x| x.reverse!}
+  return reversed_array.join(" ")
+end
+
+# FIRST REFACTORED CODE
+def reverse_each_word(array)
+  new_array = array.split(" ")
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
+end
