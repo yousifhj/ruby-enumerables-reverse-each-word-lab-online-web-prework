d2e92775f3b70be5075a804e853986bd7fe5486c
Huda Yousif <hjdoski@gmail.com>
2019-10-01 18:56:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,3 @@
 def reverse_each_word(array)
-  new_array = array.split(" ")
-  reversed_array = new_array.each {|x| x.reverse!}
-  return reversed_array.join(" ")
-end
-
-
-def reverse_each_word(array)
-  new_array = array.split(" ")
-  new_array.collect {|x| x.reverse!}
-  new_array.join(" ")
+  array.split(" ").collect {|x| x.reverse!}.join(" ")
 end
